127a94ed5857fd13132a7e1e41069e62ba3fd2fd
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-02-28 06:07:45 -0500

    first commit

---
+++ diccionario-main/diccionario.py
@@ -0,0 +1,46 @@
+#definir las variables y establecer los diccionarios con los productos
+
+menu = dict ({
+    "Pan-Dulces": {
+        "producto": list([
+    {"nombre":'Pan de canela', "valor": 2.50},
+    {"nombre":'Bollo de chocolate', "valor": 3.00},
+    {"nombre":'Rosquillas glaseadas', "valor": 2.75},
+    {"nombre":'Pan de plátano', "valor": 2.25},
+    {"nombre":'Concha', "valor": 2.50},
+    {"nombre":'Pandemuerto', "valor": 2.80},
+    {"nombre":'Trenza de frutas', "valor": 3.25},
+    {"nombre":'Pan de jengibre', "valor": 3.50},
+    {"nombre":'Bollos de pasas', "valor": 2.90},
+    {"nombre":'Donas de vainilla', "valor": 2.75}
+        ]),
+
+        "Pan-Salado":list([
+    {"nombre":'Baguette',"valor": 2.00},
+    {"nombre":'Pretzel',"valor": 2.20},
+    {"nombre":'Focaccia',"valor": 2.50},
+    {"nombre":'Pan de ajo',"valor": 2.25},
+    {"nombre":'Pan de centeno',"valor": 2.30},
+    {"nombre":'Ciabatta',"valor": 2.40},
+    {"nombre":'Bollos de perrito caliente',"valor": 2.50},
+    {"nombre":'Pan de maíz',"valor": 2.10},
+    {"nombre":'Pan de pita',"valor": 2.20},
+    {"nombre":'Panecillos de queso',"valor": 2.75}
+        ])
+   
+}), 
+                "Postres" : list([
+   {"nombre":"Pastel de Chocolate", "valor": 25.99},
+   {"nombre":"Helado de Vainilla", "valor": 12.50},
+   {"nombre":"Tarta de Manzana", "valor": 18.75},
+   {"nombre":"Cupcakes de Fresa", "valor": 8.99},
+   {"nombre":"Mousse de Limón", "valor": 14.25},
+   {"nombre":"Brownie de Nuez", "valor": 10.50},
+   {"nombre":"Gelatina de Frutas", "valor": 6.99},
+   {"nombre":"Cheesecake de Frambuesa", "valor": 22.00},
+   {"nombre":"Profiteroles de Chocolate", "valor": 16.80},
+   {"nombre":"Arroz con Leche", "valor": 9.25}
+        ])
+    }
+})
+
